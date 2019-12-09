67a498fde023941da11701b143e7e01eb9ab3d6f
BR-liu <liuxin999999@sohu.com>
2019-12-09 15:47:15 +0800

    提交使用bulder重构之后的启动类

--- market-api/src/main/java/com/brliu/Application.java
+++ market-api/src/main/java/com/brliu/Application.java
@@ -1,8 +1,9 @@
 package com.brliu;
 
 import com.brliu.annotation.EnableCorsConfiguration;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
@@ -10,6 +11,9 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "com.brliu.mapper")
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+        //SpringApplication.run(Application.class, args);
     }
 }
